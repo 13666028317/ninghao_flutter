d2e2692d67c2dc5ff9519be2ddd0921a43bd8dd6
zhulonghai <zhulonghai@ffrj.net>
2020-01-25 21:50:29 +0800

    tabbar/tabView

---
+++ lib/pages/listview-demo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class ListViewDemo extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+              ),
+              SizedBox(height: 16.0),
+              Text(posts[index].title,
+                  style: Theme.of(context).textTheme.title),
+              Text(posts[index].author,
+                  style: Theme.of(context).textTheme.subhead),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilder,
+    );
+  }
+}
